3e34f7206380c31a198bb46b68c7812efc628d3c
Jared <jaredwads@gmail.com>
2016-02-22 23:04:16 -0700

    Added unified test class

---
+++ Test/RunAllTests.py
@@ -0,0 +1,12 @@
+from Test import TestTwitterDataAccessObject
+from Test import TestTwitterAPI
+from Test import TestTwitterMongo
+
+print "\nTestTwitterDataAccessObject"
+TestTwitterDataAccessObject.main()
+
+print "\nTestTwitterAPI"
+TestTwitterAPI.main()
+
+print "\nTestTwitterMongo"
+TestTwitterMongo.main()
