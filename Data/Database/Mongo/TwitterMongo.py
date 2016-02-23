04ff9a3241d244d80ea7c3e00d0307f8546f8c7e
Jared <jaredwads@gmail.com>
2016-02-22 23:04:00 -0700

    fleshed out twitter mongo

--- Data/Database/Mongo/TwitterMongo.py
+++ Data/Database/Mongo/TwitterMongo.py
@@ -10,16 +10,48 @@ class TwitterMongo:
         self.statuses = db.Statuses
 
     def save_user(self, user):
-        if self.get_user(user.id_str) is None:
-            self.users.insert_one(user._json).inserted_id
+        return self.users.insert_one(user._json).inserted_id
+
+    def save_users(self, users):
+        for user in users:
+            self.save_user(user)
 
     def save_status(self, status):
-        if self.get_status(status.id_str) is None:
-            self.statuses.insert_one(status._json).inserted_id
+        if self.get_user(status.user.id_str) is None:
             self.save_user(status.user)
+        return self.statuses.insert_one(status._json).inserted_id
+
+    def save_statuses(self, statuses):
+        for status in statuses:
+            self.save_status(status)
 
     def get_user(self, user_id):
         return self.users.find_one({"id_str": "{0}".format(user_id)})
 
     def get_status(self, status_id):
         return self.statuses.find_one({"id_str": "{0}".format(status_id)})
+
+    def get_user_statuses(self, user_id):
+        return self.statuses.find({"user.id_str": str(user_id)})
+
+    def get_followers(self, user_id):
+        user = self.get_user(user_id)
+        if "followers" in user:
+            return user['followers']
+        return None
+
+    def get_following(self, user_id):
+        user = self.get_user(user_id)
+        if "following" in user:
+            return user['following']
+        return None
+
+    def set_followers(self, user_id, followers):
+        user = self.get_user(user_id)
+        for follow in followers:
+            self.users.update({"_id": user["_id"]}, {"$addToSet": {"followers": follow._json['id_str']}})
+
+    def set_following(self, user_id, following):
+        user = self.get_user(user_id)
+        for follow in following:
+            self.users.update({"_id": user["_id"]}, {"$addToSet": {"following": follow._json['id_str']}})
